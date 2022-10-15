db7e89fdd2f2af72cd0f0d2c0b3cd00def12fc95
Howard Roatti <hroatti@hotmail.com>
2022-10-15 14:50:19 -0300

    Criação de novos menus

--- utils/config.py
+++ utils/config.py
@@ -7,21 +7,32 @@ MENU_PRINCIPAL = """Menu Principal
 """
 
 MENU_RELATORIOS = """Relatórios
-1 - Relatório de Fornecedores
-2 - Relatório de Pedidos
-3 - Relatório de Produtos
-4 - Relatório de Clientes
-5 - Relatório de Fornecedores
-6 - Relatório de Itens de Pedidos
+1 - Relatório de Clientes
+2 - Relatório de Agenda
+3 - Relatório de Laboratórios
 0 - Sair
 """
 
 MENU_ENTIDADES = """Entidades
-1 - PRODUTOS
-2 - CLIENTES
-3 - FORNECEDORES
-4 - PEDIDOS
-5 - ITENS DE PEDIDOS
+1 - CLIENTES
+2 - AGENDA
+3 - LABORATORIOS
+"""
+
+MENU_ATRIBUTOS_CLIENTES = """Atributos de Clientes
+- NOME
+- TELEFONE
+"""
+
+MENU_ATRIBUTOS_AGENDA = """Atributos de Clientes
+- DATA
+- HORA INICIO
+- HORA FIM
+"""
+
+MENU_ATRIBUTOS_LABORATORIOS = """Atributos de Clientes
+- QUANTIDADE DE MÁQUINAS
+- TIPO DE LABORATORIO
 """
 
 # Consulta de contagem de registros por tabela
