8c7338f32c300e55c978da3305c948d6b3fc5233
Howard Roatti <hroatti@hotmail.com>
2022-10-15 14:50:05 -0300

    Crianção do arquivo principal para rodar os menus

---
+++ principal.py
@@ -0,0 +1,95 @@
+from utils import config
+
+def menu_relatorio():
+    print(config.MENU_RELATORIOS)
+    choice = int(input())
+    config.clear_console(1)
+
+    if choice == 1:
+        print("relatório 1")
+    elif choice == 2:
+        print("relatório 2")
+    elif choice == 0:
+        print("")
+    else:
+        print('opção inválida')
+
+def menu_inserir():
+    config.clear_console(1)
+    print(config.MENU_ENTIDADES)
+    choice = int(input('Escolha uma entidade\n'))
+    config.clear_console(1)
+
+    if choice == 1:
+        print(config.MENU_ATRIBUTOS_CLIENTES)
+    elif choice == 2:
+        print(config.MENU_ATRIBUTOS_AGENDA)
+    elif choice == 3:
+        print(config.MENU_ATRIBUTOS_LABORATORIOS)
+    elif choice == 4:
+        print("")
+    else:
+        print('opção inválida')
+
+def menu_remover():
+    config.clear_console(1)
+    print(config.MENU_ENTIDADES)
+    choice = int(input('Escolha uma entidade\n'))
+    config.clear_console(1)
+
+    if choice == 1:
+        print(config.MENU_ATRIBUTOS_CLIENTES)
+    elif choice == 2:
+        print(config.MENU_ATRIBUTOS_AGENDA)
+    elif choice == 3:
+        print(config.MENU_ATRIBUTOS_LABORATORIOS)
+    elif choice == 4:
+        print("")
+    else:
+        print('opção inválida')
+
+def menu_atualizar():
+    config.clear_console(1)
+    print(config.MENU_ENTIDADES)
+    choice = int(input('Escolha uma entidade\n'))
+    config.clear_console(1)
+
+    if choice == 1:
+        print(config.MENU_ATRIBUTOS_CLIENTES)
+    elif choice == 2:
+        print(config.MENU_ATRIBUTOS_AGENDA)
+    elif choice == 3:
+        print(config.MENU_ATRIBUTOS_LABORATORIOS)
+    elif choice == 4:
+        print("")
+    else:
+        print('opção inválida')
+
+def run():
+    while True:
+        print(config.MENU_PRINCIPAL)
+        choice = int(input())
+        config.clear_console(1)
+        
+        if choice == 1: #Relatórios
+            menu_relatorio()
+        
+        elif choice == 2: #Inserir registros
+            menu_inserir()
+
+        elif choice == 3: #Remover registros
+            menu_remover()
+        
+        elif choice == 4: #Atualizar registros
+            menu_atualizar()
+        
+        elif choice == 5: #Sair
+            print("#############Tchau#############")
+            exit(0)
+        else:
+            print("#############opção inválida#############")
+            exit(1)
+
+
+if __name__ == '__main__':
+    run()
