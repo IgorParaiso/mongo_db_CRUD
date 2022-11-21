5bd8e66f449efbbacabebbbf11a7a66f2743e715
igor.paraiso <igorparaisod@gmail.com>
2022-11-21 19:42:46 -0300

    atualização do controller de clientes

--- src/controller/controller_cliente.py
+++ src/controller/controller_cliente.py
@@ -1,6 +1,7 @@
 from model.clientes import Cliente
-from conexion.oracle_queries import OracleQueries
+from conexion.mongo_queries import MongoQueries
 from reports.relatorios import Relatorio
+import pandas as pd
 
 relatorio = Relatorio()
 class Controller_Cliente:
@@ -10,18 +11,18 @@ class Controller_Cliente:
     def inserir_cliente(self) -> Cliente:
         
         # Faz a conexão com o banco de dados
-        oracle = OracleQueries(can_write=True)
-        oracle.connect()
+        mongo = MongoQueries()
+        mongo.connect()
         
         print(relatorio.get_relatorio_clientes())
         cpf = int(input('Insira o cpf do cliente: '))
-        if self.verifica_se_existe(oracle, cpf):
+        if self.verifica_se_existe(mongo, cpf):
             nome = input('Insira o nome do cliente: ')
             telefone = input('Insira o telefone do cliente: ')
 
-            oracle.write(f"insert into clientes values ({cpf},'{nome}', '{telefone}')")
-
-            df_cliente = oracle.sqlToDataFrame(f"select id_cliente, nome_cliente, telefone from clientes where id_cliente = {cpf}")
+            mongo.db["cliente"].insert_one({"cpf":cpf, "nome":nome, "telefone": telefone})
+            
+            df_cliente = pd.read_json(mongo.db['cliente'].find_one({"cpf":cpf}))
 
             novo_cliente = Cliente(df_cliente.id_cliente.values[0], df_cliente.nome_cliente.values[0], df_cliente.telefone.values[0])
 
@@ -34,28 +35,28 @@ class Controller_Cliente:
 
     def atualizar_cliente(self) -> Cliente:
         # Faz a conexão com o banco de dados
-        oracle = OracleQueries(can_write=True)
-        oracle.connect()
+        mongo = MongoQueries()
+        mongo.connect()
 
         print(relatorio.get_relatorio_clientes())
         cpf = int(input("Insira o CPF do cliente a ser alterado: "))
 
-        if not self.verifica_se_existe(oracle, cpf):
+        if not self.verifica_se_existe(mongo, cpf):
             choice = int(input("Escolha uma opção\n1 - Alterar nome\n2 - Alterar telefone\n"))
             
             if choice == 1:
                 novo_nome = input("Digite o novo nome: ")
-                oracle.write(f"update clientes set nome_cliente = '{novo_nome}' where id_cliente = '{cpf}'")
+                mongo.db['clientes'].update_one({"cpf":cpf}, {"nome":novo_nome})
 
-                df_cliente = oracle.sqlToDataFrame(f"select id_cliente, nome_cliente, telefone from clientes where id_cliente = '{cpf}'")
+                df_cliente = pd.read_json(mongo.db["clientes"].find_one({"cpf":cpf}))
                 cliente_atualizado = Cliente(df_cliente.id_cliente.values[0], df_cliente.nome_cliente.values[0], df_cliente.telefone.values[0])
                 print(cliente_atualizado.to_string())
                 return cliente_atualizado
             
             elif choice == 2:
                 novo_telefone = input("Digite o novo telefone: ")
-                oracle.write(f"update clientes set telefone = '{novo_telefone}' where id_cliente = '{cpf}'")
-                df_cliente = oracle.sqlToDataFrame(f"select id_cliente, nome_cliente, telefone from clientes where id_cliente = '{cpf}'")
+                mongo.db['clientes'].update_one({"cpf":cpf}, {"telefone":novo_telefone})
+                df_cliente = pd.read_json(mongo.db["clientes"].find_one({"cpf":cpf}))
                 cliente_atualizado = Cliente(df_cliente.id_cliente.values[0], df_cliente.nome_cliente.values[0], df_cliente.telefone.values[0])
                 print(cliente_atualizado.to_string())
                 return cliente_atualizado
@@ -69,20 +70,20 @@ class Controller_Cliente:
     
     def excluir_cliente(self):
         # Faz a conexão com o banco de dados
-        oracle = OracleQueries(can_write=True)
-        oracle.connect()
+        mongo = MongoQueries()
+        mongo.connect()
 
         print(relatorio.get_relatorio_clientes())
         cpf = int(input("Insira o CPF do cliente a ser excluído: "))
 
-        if not self.verifica_se_existe(oracle, cpf):
+        if not self.verifica_se_existe(mongo, cpf):
 
-            if self.verifica_se_existe_agenda(oracle, cpf):
+            if self.verifica_se_existe_agenda(mongo, cpf):
                 
                 confirmation = str(input("Tem certeza que quer excluir esse cleinte? (Digite S para sim e N para não) "))
                 if confirmation.upper() == "S":
-                    df_cliente = oracle.sqlToDataFrame(f"select id_cliente, nome_cliente, telefone from clientes where id_cliente = '{cpf}'")
-                    oracle.write(f"delete from clientes where id_cliente = '{cpf}'")
+                    df_cliente = pd.read_json(mongo.db["clientes"].find_one({"cpf":cpf}))
+                    mongo.db["clientes"].delete_one({"cpf":cpf})
 
                     cliente_excluido = Cliente(df_cliente.id_cliente.values[0], df_cliente.nome_cliente.values[0], df_cliente.telefone.values[0])
 
@@ -91,9 +92,10 @@ class Controller_Cliente:
             
             confirmation = str(input("O usuário tem registro na tabela agenda, digite S para excluir os registros da tabela agenda e N para voltar ao menu principal: "))
             if confirmation.upper() == "S":
-                df_cliente = oracle.sqlToDataFrame(f"select id_cliente, nome_cliente, telefone from clientes where id_cliente = '{cpf}'")
-                oracle.write(f"delete from agenda where id_cliente = '{cpf}'")
-                oracle.write(f"delete from clientes where id_cliente = '{cpf}'")
+                df_cliente = pd.read_json(mongo.db["clientes"].find_one)({"cpf":cpf})
+                
+                mongo.db["agenda"].delete_many({"cpf":cpf})
+                mongo.db["clientes"].delete_one({"cpf":cpf})
 
                 cliente_excluido = Cliente(df_cliente.id_cliente.values[0], df_cliente.nome_cliente.values[0], df_cliente.telefone.values[0])
 
@@ -104,12 +106,20 @@ class Controller_Cliente:
             print(f"cliente {cpf} não existe")
             return None
 
-    def verifica_se_existe(self, oracle:OracleQueries, cpf:int=None) -> bool:
+    def verifica_se_existe(self, mongo:MongoQueries, cpf:int=None) -> bool:
 
-        df_cliente = oracle.sqlToDataFrame(f"select id_cliente, nome_cliente from clientes where id_cliente = '{cpf}'")
+        df_cliente = pd.read_json(mongo.db["cliente"].find_one({"cpf":cpf}))
         return df_cliente.empty
 
-    def verifica_se_existe_agenda(self, oracle:OracleQueries, id_agenda:int=None) -> bool:
+    def verifica_se_existe_agenda(self, mongo:MongoQueries, id_agenda:int=None) -> bool:
 
-        df_agenda = oracle.sqlToDataFrame(f"select id_agenda, id_cliente, id_lab from agenda where id_cliente = '{id_agenda}'")
+        df_agenda = pd.read_json(mongo.db["agenda"].find_one({"id_cliente": id_agenda}))
         return df_agenda.empty
+    
+
+
+
+
+
+
+    
